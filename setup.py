bf88f6565cac695dbbadf3bc66a0315cbb09ea80
bass <gtluu912@gmail.com>
2023-08-11 17:39:12 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyTDFSDK',
+      version='0.1.0',
+      description='Python wrapper for Bruker TDF-SDK',
+      url='https://github.com/gtluu/pyTDFSDK',
+      author='Gordon T. Luu',
+      author_email='gtluu912@gmail.com',
+      license='Apache License',
+      packages=['pyTDFSDK'],
+      install_requires=['numpy'])
