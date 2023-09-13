0b7ba063be2cf29e9d7cf62764a408bdae08c2a2
bass <gtluu912@gmail.com>
2023-09-13 13:30:25 -0700

    update gitignore

--- build/lib/pyTDFSDK/__init__.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-import enum
-import os
-import platform
-import numpy as np
-
-from pyTDFSDK.ctypes_data_structures import *
-from pyTDFSDK.init_tdf_sdk import *
-from pyTDFSDK.methods import *
