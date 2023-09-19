2a4dc13534b6ad0c3a354ecb20d1e294eb26c6f7
bass <gtluu912@gmail.com>
2023-09-18 17:04:10 -0700

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyTDFSDK',
-      version='0.1.6',
+      version='0.2.0',
       description='Python wrapper for Bruker TDF-SDK',
       url='https://github.com/gtluu/pyTDFSDK',
       author='Gordon T. Luu',
